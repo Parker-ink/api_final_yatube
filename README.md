40c8a4c79d756d8830bd84142a642709d4b2f5c1
Вадим Ковалев <minenikolasspace@bk.ru>
2022-09-22 16:42:26 +0300

    My first try

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# api_final
-api final
+Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+
+https://github.com/boreesych/kittygram_backend.git
+cd kittygram_backend
+Cоздать и активировать виртуальное окружение:
+
+python -m venv env
+source env/bin/activate
+python -m pip install --upgrade pip
+Установить зависимости из файла requirements.txt:
+
+pip install -r requirements.txt
+Выполнить миграции:
+
+python manage.py migrate
+Запустить проект:
+
+python manage.py runserver
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,65 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters, permissions, status, viewsets, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permission import IsOwnerOrReadOnly
+from .serializers import (PostSerializer, FollowSerializer,
+                          CommentSerializer, GroupSerializer)
+from posts.models import Post, Group, User
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['group', ]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def create(self, request):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+
+class CreateListViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class FollowViewSet(CreateListViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user.username)
+        return user.follower
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
